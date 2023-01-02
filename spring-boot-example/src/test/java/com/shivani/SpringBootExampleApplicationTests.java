53c10483ea1baf9f71bba2041947e055259883e7
Shivani31996 <shivani.chavan96@gmail.com>
2023-01-02 13:31:28 -0500

    Initial Commit

---
+++ spring-boot-example/src/test/java/com/shivani/SpringBootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
