bfff08dd47cdd6a0df8e32abec58a5262623556a
Shivani31996 <shivani.chavan96@gmail.com>
2023-01-02 16:52:51 -0500

    Committing boiler-plate code

---
+++ spring-boot-example/src/main/java/com/shivani/Main.java
@@ -0,0 +1,61 @@
+package com.shivani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Objects;
+
+@SpringBootApplication
+@RestController
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class,args);
+    }
+
+    @GetMapping("/greet")
+    public GreetResponse greet(){
+        GreetResponse response = new GreetResponse("Hello",
+                List.of("Java", "Golang", "JavaScript"),
+                new Person("Alex",28,30_000));
+        return  response;
+    }
+
+    record Person(String name,int age, double savings) {
+
+    }
+    record GreetResponse(String greet,
+                         List<String> favProgrammingLanguages, 
+                         Person person){}
+
+//This is the equivalent code of record statement above
+//    class GreetResponse{
+//        private final String greet;
+//        public String getGreet() {
+//            return greet;
+//        }
+//        GreetResponse(String greet){
+//            this.greet = greet;
+//        }
+//
+//        @Override
+//        public String toString() {
+//            return super.toString();
+//        }
+//
+//        @Override
+//        public boolean equals(Object o) {
+//            if (this == o) return true;
+//            if (o == null || getClass() != o.getClass()) return false;
+//            GreetResponse that = (GreetResponse) o;
+//            return Objects.equals(greet, that.greet);
+//        }
+//
+//        @Override
+//        public int hashCode() {
+//            return Objects.hash(greet);
+//        }
+//    }
+}
